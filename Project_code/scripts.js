c2fdb8ea0bc0d4769f010595466300a82ff84de1
Goodwill Phooko <1886121@students.wits.ac.za>
2022-05-17 03:08:30 +0200

    adding a letter to game board

--- Project_code/scripts.js
+++ Project_code/scripts.js
@@ -7,9 +7,42 @@ for (let i = 0; i < 6; i++) {
   row.className = 'letter-row'
 
   for (let j = 0; j < 5; j++) {
-    const box = document.createElement('div')
+    const box = document.createElement('li')
     box.className = 'letter-box'
-    row.appendChild(box)
+    row.append(box)
   }
   board.appendChild(row)
+}
+// ---------- Handling user Input ------------
+const numberOfguesses = 0
+let nextLetter = 0
+const word = []
+
+document.querySelectorAll('.keyboard-button').forEach(item => {
+  item.addEventListener('click', event => {
+    // handle clicking
+    const clickedLetter = item.textContent
+
+    // Check for deleting
+    if (clickedLetter === 'Del') {
+      // erase a letter
+    } else if (clickedLetter === 'Enter') {
+      /// go to next line
+    } else {
+      insertLetter(clickedLetter)
+      console.log(item.innerHTML)
+      if (word.length < 5) { /// verify length is still less than five
+        console.log(word)
+        word.push(clickedLetter)
+      }
+    }
+  })
+})
+/// Inserting a letter
+function insertLetter (letter) {
+  const row = document.getElementsByClassName('letter-row')[numberOfguesses]
+  const box = row.children[nextLetter]
+  box.textContent = letter
+  box.classList.add('filled-box')
+  nextLetter += 1
 }
