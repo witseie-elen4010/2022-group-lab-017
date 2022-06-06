cd56b025fba121f655cdb2c308ef09a1cd12c631
Goodwill Phooko <1886121@students.wits.ac.za>
2022-06-06 21:45:23 +0200

    fixing route 2

--- Project_code/Backend/Routes/userAccount.js
+++ Project_code/Backend/Routes/userAccount.js
@@ -3,8 +3,8 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../Controllers/userAccount')
 
-router.post('/register', userController.createAccount);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
+router.post('/register', userController.createAccount)
+router.post('/signup', userController.login)
+router.post('/logout', userController.logout)
 
 module.exports = router
